d5f66e3837ba917a433dc9c26b18b3088f503008
Flumuffel <luca.weber.gelti@gmail.com>
2020-08-20 20:36:32 +0200

    Update animemes

---
+++ commands/animeme.js
@@ -0,0 +1,24 @@
+const randomPuppy = require('random-puppy')
+
+module.exports = {
+    name: "animeme",
+    aliases: ["anime"],
+    categories: "Fun",
+    description: "Returns a epic meme",
+    usage: "",
+    run: (client, message, args) => {
+
+        const subReddits = ['DnDAnimemes', 'animememes', 'Animemes', 'animemebank', 'animememes', "wholesomeanimemes", "MemesOfAnime"]
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)]
+
+        const img = randomPuppy(random)
+        const embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`http://reddit.com/r/${random}`)
+
+        message.channel.send(embed)
+
+    }
+}
