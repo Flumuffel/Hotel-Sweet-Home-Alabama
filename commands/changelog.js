fa8c3f318050d7d47e5e0ece6164e2d6b7117bbe
Flumuffel <luca.weber.gelti@gmail.com>
2020-08-21 17:27:24 +0200

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -17,7 +17,7 @@ module.exports = {
         }
 
         request(options, function (error, response, body) {
-            message.channel.send(body);
+            message.channel.send(JSON.parse(body)[0]);
         });
 
     }
