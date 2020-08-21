0d1ac9eab1a12128f9db02c6f18aae13fdc24775
Flumuffel <luca.weber.gelti@gmail.com>
2020-08-21 17:45:30 +0200

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,5 +1,6 @@
 var request = require('request');
 const { MessageEmbed } = require("discord.js");
+const { config } = require('dotenv/types');
 
 module.exports = {
     name: "changelog",
@@ -17,8 +18,38 @@ module.exports = {
         }
 
         request(options, function (error, response, body) {
-            message.channel.send(JSON.parse(body));
-            console.log(JSON.parse(body)[0]);
+            let config = JSON.parse(body)
+            let field = []
+
+            for (i = 0; i < config.length; i++) {
+                let a = {
+                    name: `**[${config[i].tag_name}] ${config[i].name}**`,
+                    value: `**${config[i].body}**`,
+                    inline: false
+                }
+                
+                array_psuh($field, a);
+            }
+
+            return message.channel.send({
+                embed: {
+                    title: "Info",
+                    color: 0x1D82B6,
+                    fields: field
+                }
+            })
+
+            // {
+            //     name: `**Version**`,
+            //         value: `**${client.ver}**`,
+            //             inline: false
+            // },
+            // {
+            //     name: `**Developer**`,
+            //         value: `**<@274209471968837634>**`,
+            //             inline: false
+            // }
+
         });
 
     }
