1cb70606200a63bfef1d0808ef83d2657eed04e7
Flumuffel <luca.weber.gelti@gmail.com>
2020-08-21 17:18:52 +0200

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -8,7 +8,7 @@ module.exports = {
     description: "Send you the changelog of the bot",
     usage: "",
     run: (client, message, args) => {
-        
+
         var options = {
             url: 'https://api.github.com/repos/Flumuffel/Hotel-Sweet-Home-Alabama/releases',
             headers: {
@@ -16,28 +16,8 @@ module.exports = {
             }
         }
 
-        request(options , function (error, response, body) {
-            if( error === "null") {
-                message.channel.send(body.json());
-            } else {
-                console.log(error)
-                return message.channel.send({
-                    embed: {
-                        title: "Error",
-                        color: 0xfc0303,
-                        fields: [{
-                            name: `**Error**`,
-                            value: `**${error}**`,
-                            inline: false
-                        },
-                        {
-                            name: `**statusCode**`,
-                            value: `**${response && response.statusCode}**`,
-                            inline: false
-                        }]
-                    }
-                })
-            }
+        request(options, function (error, response, body) {
+            message.channel.send(body.json());
         });
 
     }
