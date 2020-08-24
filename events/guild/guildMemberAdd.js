7d8870a897e1412ef73d51d44a83b961a5f43050
Flumuffel <luca.weber.gelti@gmail.com>
2020-08-24 20:49:35 +0200

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,4 +1,5 @@
 module.exports = (guild, member) => {
     console.log(`${member.user.username} joined!`);
-    guild.channels.cache.find('739287222980968478').send("<@409394223352119296>")
+    const channel = client.channels.cache.find(channel => channel.name === "☜💍freischaltung💍☞")
+    channel.send("<@409394223352119296>")
 }
