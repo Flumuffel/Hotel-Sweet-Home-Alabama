7a3a208819f3c1fac46313f715bf630debd5c355
Flumuffel <luca.weber.gelti@gmail.com>
2020-08-24 20:20:16 +0200

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,3 +1,4 @@
 module.exports = (guild, member) => {
-    console.log(`${member.user.username} joined ${guild.name}`);
+    console.log(`${member.user.username} joined!`);
+    guild.channels.cache.find('739287222980968478').send("<@409394223352119296>")
 }
