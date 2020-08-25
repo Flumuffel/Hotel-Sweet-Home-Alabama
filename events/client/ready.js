0e33a922ed84545824fe139bbedf769c442666b0
Flumuffel <luca.weber.gelti@gmail.com>
2020-08-25 19:30:58 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,24 @@
+var request = require('request');
+var version;
+
+var options = {
+    url: 'https://api.github.com/repos/Flumuffel/Hotel-Sweet-Home-Alabama/releases',
+    headers: {
+        'User-Agent': 'request'
+    }
+}
+request(options, function (error, response, body) {
+    let config = JSON.parse(body)
+    version = config[0].tag_name;
+});
+
 module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setStatus('idle')
+    client.user.setPresence({
+        status: "idle",  //You can show online, idle....
+        game: {
+            name: `${version}`,  //The message shown 
+            type: "STREAMING" //PLAYING: WATCHING: LISTENING: STREAMING:
+        }
+    });
 }
