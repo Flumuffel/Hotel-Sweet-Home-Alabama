75af88d4e97f992398aa359f085d33e0b6100381
Flumuffel <luca.weber.gelti@gmail.com>
2020-08-25 19:13:25 +0200

    Roleinfo command

---
+++ commands/roleinfo.js
@@ -0,0 +1,54 @@
+module.exports = {
+    name: "roleinfo",
+    aliases: ["rli"],
+    categories: "Info",
+    description: "Informationen über rollen.",
+    usage: "",
+    run: (client, message, args) => {
+
+        if(!args[0]) return message.channel.send("Bitte gib ein Rollen name ein oder eine id!")
+
+        let desc = ""
+        let role;
+        if (isNaN(args[0])) {
+            role = message.guild.roles.cache.find(role => role.name === args[0]);
+        } else {
+            role = message.guild.roles.cache.find(role => role.id === args[0]);
+        }
+
+        if (role == undefined) {
+            return message.channel.send({
+                embed: {
+                    title: "RoleInfo",
+                    color: 0xa83232,
+                    description: `The role ${args[0]} doesn't exist on this server.`
+                }
+            
+            })
+        }
+
+        let membersOfRole = role.members;
+
+        membersOfRole.forEach(user => {
+            desc += `- ${user.user.username}\n`
+        });
+
+        return message.channel.send({
+            embed: {
+                title: `${role.name}`,
+                color: 0x1D82B6,
+                fields: [{
+                    name: `**User**`,
+                    value: `${desc}`,
+                    inline: true
+                },
+                {
+                    name: `**Member**`,
+                    value: `${membersOfRole.size}`,
+                    inline: true
+                }]
+            }
+        })
+
+    }
+}
--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = (guild, member) => {
     console.log(`${member.user.username} joined!`);
-    const channel = guild.channels.cache.find(channel => channel.name === "☜💍freischaltung💍☞")
+    const channel = guild.channels.cache.find(channel => channel.id === "739287222980968478")
     channel.send("<@409394223352119296>")
 }
