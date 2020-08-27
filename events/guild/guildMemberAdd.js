3a5a773aa319d0dbedc7ec8ab6ffaba54fe34fbe
Flumuffel <40270803+Flumuffel@users.noreply.github.com>
2020-08-27 09:58:51 +0200

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,5 +1,7 @@
 module.exports = (guild, member) => {
     console.log(`${member.user.username} joined!`);
+    console.log(`Is bot: ${member.user.bot`);
+    if(member.user.bot) return;
     const channel = guild.channels.cache.find(channel => channel.id === "739287222980968478")
     channel.send("<@409394223352119296>")
 }
