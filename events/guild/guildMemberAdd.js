cb3999a466d0fc63e0a906d2b1ec733fe5c74fe9
Flumuffel <40270803+Flumuffel@users.noreply.github.com>
2020-09-04 17:45:50 +0200

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -2,5 +2,5 @@ module.exports = (guild, member) => {
     console.log(`${member.user.username} joined! Is bot: ${member.user.bot}`);
     if (member.user.bot) return;
     const channel = guild.channels.cache.find(channel => channel.id === "739287222980968478")
-    channel.send(`User <@${member.id}> joined the guild! <@&747392230280527873>`)
+    channel.send(`Hallo <@${member.id}>, und wilkommen auf unserem Server. Bitte joine einmal den vorstellungs vc. Danke! <@&747392230280527873>`)
 }
